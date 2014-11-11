6985be7d9da4de198f49db0774389ff1cac28628
Max Bernstein <tekknolagi@gmail.com>
2014-11-11 16:46:39 -0500

    remove test... no tests for now

--- Library/Formula/ssock.rb
+++ Library/Formula/ssock.rb
@@ -9,17 +9,4 @@ class Ssock < Formula
     system "make"
     system "make install"
   end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test ssock`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "#{ENV['CC']} examples/af_unix/cat.c libssock.a -o examples/af_unix/cat"
-  end
 end
