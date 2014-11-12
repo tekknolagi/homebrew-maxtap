165e333649f93abcf6cd1f88f7b200276e7dfe2e
Max Bernstein <tekknolagi@gmail.com>
2014-11-11 21:06:26 -0500

    carp works... deparallelize :)

--- Formula/carp.rb
+++ Formula/carp.rb
@@ -2,10 +2,12 @@ require "formula"
 
 class Carp < Formula
   homepage "https://github.com/tekknolagi/carp"
-  url "https://github.com/tekknolagi/carp/archive/v0.1.tar.gz"
-  sha1 "b3218aa50bbf8a608a6047df0b312a8dea280429"
+  head "https://github.com/tekknolagi/carp.git"
+  url "https://github.com/tekknolagi/carp.git"
+  version 'v0.1.1'
 
   def install
+    ENV.deparallelize
     system "make"
     system "make install"
   end
